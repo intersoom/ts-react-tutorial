3e2bfa1b34e624df1a7edc575b051012eaf8012f
intersoom <sallybig@naver.com>
2022-07-21 20:38:08 +0900

    상태 관리 관련 자료

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import Greetings from "./Greetings";
+import Counter from "./Counter";
+import MyForm from "./MyForm";
 
 function App() {
   const onClick = (name: string) => {
     console.log(`${name} says hello`);
   };
-  return <Greetings name="Hello" onClick={onClick} />;
+
+  const onSubmit = (form: { name: string; description: string }) => {
+    console.log(form);
+  };
+  return (
+    <>
+      <Greetings name="Hello" onClick={onClick} />
+      <Counter />
+      <MyForm onSubmit={onSubmit} />
+    </>
+  );
 }
 
 export default App;
